0ef6503e8af92e954e1ad4a809e804e91a8e3f6d
Jozef Izso <jozef.izso@gmail.com>
2026-06-02 12:32:00 +0200

    Add .NET 10 WiX layout projects

---
+++ src/net10/Wix.Legacy.Core/SystemWindowsFormsShim.cs
@@ -0,0 +1,12 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+namespace System.Windows.Forms
+{
+    /// <summary>
+    /// Portable stand-in for the legacy fabricator UI extension point.
+    /// </summary>
+    public class Control
+    {
+    }
+}
